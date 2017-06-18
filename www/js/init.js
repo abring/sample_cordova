500346723bbf5448c0983d09e50d9bab89c1cf2f
kia <kiaxar2000@gmail.com>
2017-06-18 11:00:07 +0430

    bug fixed

--- www/js/init.js
+++ www/js/init.js
@@ -1,14 +1,6 @@
 
 $(document).ready(function(){
-    abring.init(
-        {
-            "app":"ir.abring.sdk",
-            "ping_time":120,
-            "socketRetryInterval" : 50
-        }
-    );
-
-    abring.params.player.pages.other_player_profile.setParents("other_player_profile_content");
+    abring.params.player.pages.other_player_profile.parent_id = "other_player_profile_content";
     abring.params.player.pages.other_player_profile.show =
         function (other_player_id) {
             fillOtherPlayerInfo(other_player_id,
@@ -21,7 +13,7 @@ $(document).ready(function(){
             );
         };
 
-    abring.params.player.pages.my_profile.setParents("my_profile_content");
+    abring.params.player.pages.my_profile.parent_id = "my_profile_content";
     abring.params.player.pages.my_profile.show =
         function () {
             fillMyPlayerInfo(true);
@@ -29,7 +21,7 @@ $(document).ready(function(){
             $.mobile.navigate("#my_profile");
         };
 
-    abring.params.player.pages.player_mobile_other_way.setParents("player_mobile_other_way_content");
+    abring.params.player.pages.player_mobile_other_way.parent_id = "player_mobile_other_way_content";
     abring.params.player.pages.player_mobile_other_way.show =
         function () {
             abring.params.display.hidePageFunction();
@@ -42,19 +34,19 @@ $(document).ready(function(){
         };
 
 
-    abring.params.player.pages.player_mobile_register.setParents("player_mobile_register_content");
+    abring.params.player.pages.player_mobile_register.parent_id = "player_mobile_register_content";
     abring.params.player.pages.player_mobile_register.show =
         function () {
             $.mobile.navigate("#player_mobile_register");
         };
 
-    abring.params.player.pages.player_mobile_verify.setParents("player_mobile_verify_content");
+    abring.params.player.pages.player_mobile_verify.parent_id  = "player_mobile_verify_content";
     abring.params.player.pages.player_mobile_verify.show =
         function () {
             $.mobile.navigate("#player_mobile_verify");
         };
 
-    abring.params.player.pages.my_profile_update.setParents("my_profile_update_content","my_profile_update_content");
+    abring.params.player.pages.my_profile_update.parent_id = "my_profile_update_content","my_profile_update_content";
     abring.params.player.pages.my_profile_update.show =
         function () {
             $.mobile.navigate("#my_profile_update");
@@ -68,19 +60,25 @@ $(document).ready(function(){
 
 
 
-    abring.params.message.pages.unicast.setParents("message_page_content");
+    abring.params.message.pages.unicast.parent_id = "message_page_content";
     abring.params.message.pages.unicast.show = function (player_info) {
         $.mobile.navigate("#message_page");
     };
 
-    abring.params.message.pages.list.setParents("message_list_content");
+    abring.params.message.pages.list.parent_id = "message_list_content";
     abring.params.message.pages.list.show = function () {
         $.mobile.navigate("#message_list");
     };
 
-    abring.params.message.pages.view.setParents("message_view_content");
+    abring.params.message.pages.view.parent_id = "message_view_content";
     abring.params.message.pages.view.show = function () {
         $.mobile.navigate("#message_view");
     };
+
+    abring.params.app = "ir.abring.sdk";
+    abring.params.ping_time = 120;
+    abring.params.socketRetryInterval = 50;
+
+    abring.init();
 
 });
